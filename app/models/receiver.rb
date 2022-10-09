dcaa7c105766aba7a8c277d31a99ae55df5cc036
Leonardo-teodoro <71471662+Leonardo-teodoro@users.noreply.github.com>
2022-10-09 00:40:30 -0300

    feature: add validations

--- app/models/receiver.rb
+++ app/models/receiver.rb
@@ -1,3 +1,6 @@
 class Receiver < ApplicationRecord
   has_one :category
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, presence: true
+  validates :name, presence: true
+
 end
--- app/models/residue.rb
+++ app/models/residue.rb
@@ -1,2 +1,6 @@
 class Residue < ApplicationRecord
+  has_one_attached :image, dependent: :destroy
+  validates :title, presence: true
+  validates :subtitle, presence: true
+
 end
